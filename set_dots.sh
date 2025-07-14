0ea13e716e1f3fa2e2b314ca8b3236f9a3cb669d
Tycho Brouwer <tychobrouwer33@gmail.com>
2025-07-14 13:25:01 +0200

    fix create themes dir

--- set_dots.sh
+++ set_dots.sh
@@ -8,9 +8,11 @@ find . -type d -exec mkdir -p $HOME/{} \;
 # Create links to dotfiles
 stow --adopt --restow -t "$HOME" .
 
+mkdir -p "$HOME/.local/share/themes"
 git clone https://github.com/tychobrouwer/everblush-gtk.git "$HOME/Projects/everblush-gtk"
 ln -s "$HOME/Projects/everblush-gtk" "$HOME/.local/share/themes/everblush-gtk"
 
+mkdir -p "$HOME/.local/share/icons"
 git clone https://github.com/tychobrouwer/papirus-icon-theme.git "$HOME/Projects/papirus-icon-theme"
 ln -s "$HOME/Projects/papirus-icon-theme/Papirus-Dark" "$HOME/.local/share/icons/papirus"
 ./dolphin-folder-color.sh -c cyan -d "$HOME/.local/share/icons/papirus"
