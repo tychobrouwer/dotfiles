1ecf6683f7eba6d155eb29d4ccc1545761317f10
Tycho Brouwer <tychobrouwer33@gmail.com>
2025-07-14 09:46:14 +0200

    add nvim everblush theme

---
+++ .config/nvim/lua/config/plugins/theme.lua
@@ -0,0 +1,17 @@
+-- return {
+--     "catppuccin/nvim",
+--     name = "catppuccin",
+--     priority = 1000,
+--     config = function()
+--       vim.cmd("colorscheme catppuccin-macchiato")
+--     end
+-- }
+
+return {
+  "Everblush/nvim",
+  name = "everblush",
+  priority = 1000,
+  config = function()
+    vim.cmd("colorscheme catppuccin-macchiato")
+  end
+}
