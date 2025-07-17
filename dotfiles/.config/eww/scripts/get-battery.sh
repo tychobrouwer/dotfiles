03f38fa1370678141d41575aed516c008ad0e70b
Tycho Brouwer <tycho.tbrouwer@gmail.com>
2025-07-17 12:49:41 +0200

    fix battery indicator

--- dotfiles/.config/eww/scripts/get-battery.sh
+++ dotfiles/.config/eww/scripts/get-battery.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [[ ! -d "/sys/class/power_supply/BAT*" ]]; then
+if [[ ! -d "/sys/class/power_supply/BAT0" ]]; then
   echo ""
 else
   FILES=$(ls /sys/class/power_supply/BAT*/capacity)
